7d3ce723c058b0ea4a98b2522d71abdbc1bdc164
Joonas Hiltunen <joonas.hiltunen@vincit.fi>
2024-01-03 22:31:29 +0200

    Add Sitemap and robots.txt

---
+++ src/app/sitemap.ts
@@ -0,0 +1,18 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: "https://joonashiltunen.fi",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1,
+    },
+    {
+      url: "https://joonashiltunen.fi/resume.pdf",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.5,
+    },
+  ];
+}
