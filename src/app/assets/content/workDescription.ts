36a795ff5dbde7dd57bb378dec5e0f2f45e2f556
Joonas Hiltunen <joonas.hiltunen@vincit.fi>
2024-01-04 11:56:13 +0200

    Update work experience

--- src/app/assets/content/workDescription.ts
+++ src/app/assets/content/workDescription.ts
@@ -6,8 +6,9 @@ export const workDescription = () => {
         title: "Software Developer",
         years: "2022-present",
         description: [
-          "Worked at a well-known company creating a CIS for Energy sector",
+          "Worked at a well-known company in the energy sector developing a Customer Information System for energy companies",
           "- Worked on the reporting module of the customer's flagship CIS product",
+          "- Worked on modifying the module for a new foreign market",
           "- Developed a cutting edge reporting microservice for customer's new software product",
         ],
         technologies: [
