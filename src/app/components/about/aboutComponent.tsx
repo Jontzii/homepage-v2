74d3b85d812a97fc00d0c3d2ac5fe1c11ebcc8f8
Joonas Hiltunen <joonas.hiltunen@vincit.fi>
2025-04-13 20:42:11 +0300

    Improve image handling

--- src/app/components/about/aboutComponent.tsx
+++ src/app/components/about/aboutComponent.tsx
@@ -11,13 +11,15 @@ export default function AboutComponent() {
       className="w-full h-full text-center pt-12 lg:pt-24 pb-12 lg:pb-24 flex flex-col lg:flex-row justify-center items-center"
     >
       <div className="w-full lg:w-1/2 p-4 flex justify-center lg:justify-end">
-        <div className="-full md:w-4/5 lg:w-full xl:w-4/5 2xl:w-4/5 mr-0 lg:ml-8 lg:mr-4 xl:ml-16 xl:mr-16 flex flex-row justify-center">
+        <div className="w-full md:w-4/5 lg:w-full xl:w-4/5 2xl:w-4/5 mr-0 lg:ml-8 lg:mr-4 xl:ml-16 xl:mr-16 flex flex-row justify-center">
           <div className="w-2/3 overflow-hidden rounded-full">
             <Image
               src={profilePic}
               alt="Picture of Joonas Hiltunen"
               placeholder="blur"
-              priority
+              priority={false}
+              sizes="(max-width: 1024px) 66vw, 33vw"
+              quality={50}
             />
           </div>
         </div>
