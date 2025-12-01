7b52cc3ac5823b1516826e6d6ada0b0d130839ac
Joonas Hiltunen <joonas.hiltunen@vincit.fi>
2025-12-01 09:56:53 +0200

    Upgrade next to v16

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    qualities: [25, 50, 75],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
